63af5f1913e0ddf2c4fc0e83eea7d1c882a07f0d
Raverbury <smurfjp198@gmail.com>
2022-03-15 16:45:57 +0700

    Added http request

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -50,14 +51,21 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  Future<int> _fetchData() async {
+    final raw = await http
+        .get(Uri.parse('https://danganhapi.herokuapp.com/api/temperature'));
+    return raw.statusCode;
+  }
+
+  void _incrementCounter() async {
+    int statusCode = await _fetchData();
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter = statusCode;
     });
   }
 
